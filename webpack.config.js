5b3668f731e5b8ab8100ec36abe08e1ec2dcff2e
Yaser <adel.mehrban@gmail.com>
2016-08-18 20:01:47 +1000

    added auto prefixer"

--- webpack.config.js
+++ webpack.config.js
@@ -29,12 +29,12 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader')
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!sass-loader')
       }
     ]
   },
